1258aa9cc773282591ea6b05f9aeeecb32c8bde7
GreyA <grey.altaer@gmail.com>
2025-03-14 09:38:58 -0700

    added landing and dashboard fuctionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 // App.jsx
-
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
+import Landing from './components/Landing/Landing.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+
+import { UserContext } from './contexts/UserContext.jsx';
 
 const App = () => {
+  const { user } = useContext(UserContext);
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
     </>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -28,6 +28,9 @@ const NavBar = () => {
                 <ul>
                     <li>Welcome, {user.username}</li>
                     <li>
+                        <Link to='/'>Dashboard</Link>
+                    </li>
+                    <li>
                         <Link to="/" onClick={handleSignOut}>
                             Sign Out
                         </Link>
@@ -36,6 +39,7 @@ const NavBar = () => {
             ) : (
                 <ul>
                     {/* The new link! */}
+                    <li><Link to='/'>Home</Link></li>
                     <li>
                         <Link to="/sign-in">Sign In</Link>
                     </li>
