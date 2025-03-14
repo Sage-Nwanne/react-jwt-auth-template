aa0df8a2fc80fad25c122814fa4aa88eb89538af
User Name <user@email.com>
2025-03-14 11:06:07 -0400

    built sign-up functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 // src/App.jsx
 
 import NavBar from './components/NavBar/NavBar';
+import SignUpForm from './components/SignUpForm/SignUpForm';
 
 const App = () => {
   
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+ 
+      <Routes>
+
+        <Route path='/sign-up' element={<SignUpForm />} />
+
+      </Routes>
     </>
   );
 };
